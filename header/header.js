4c28c78d41f6a3131c5982dc9a652a2f4b46ede8
nattipan jarunat <nattipanjarunat@gmail.com>
2026-01-08 16:47:29 +0700

    convert_header

--- header/header.js
+++ header/header.js
@@ -6,7 +6,7 @@ fetch("header/header.html")
     });
 
 function initHeader() {
-    const bannerEl = document.querySelector('.site-header');
+    const bannerEl = document.querySelector('.site-banner');
     const titleEl = document.getElementById('page-title');
 
     function setHeaderBg(bg) {
