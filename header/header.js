1bad537c96d1f17d9943b74965c0ad9dd6a949ca
nattipan jarunat <nattipanjarunat@gmail.com>
2026-01-08 14:55:56 +0700

    Header_BG

--- header/header.js
+++ header/header.js
@@ -6,7 +6,7 @@ fetch("header/header.html")
     });
 
 function initHeader() {
-    const bannerEl = document.querySelector('.site-banner');
+    const bannerEl = document.querySelector('.site-header');
     const titleEl = document.getElementById('page-title');
 
     function setHeaderBg(bg) {
